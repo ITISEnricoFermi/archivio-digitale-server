88a8682db5832aafc58e6a71beaf2a9f566da40e
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-02-17 13:29:25 +0100

    Aggiunto il comando Lint. Corretti alcuni errori.

--- server/config/env/env.js
+++ server/config/env/env.js
@@ -1,12 +1,12 @@
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development'
 
 if (!process.env.DOCKER) {
-  const config = require("./env.json");
-  const envConfig = config[env];
+  const config = require('./env.json')
+  const envConfig = config[env]
 
   Object.keys(envConfig).forEach(key => {
-    process.env[key] = envConfig[key];
-  });
+    process.env[key] = envConfig[key]
+  })
 } else {
-  process.env.CORS_WHITELIST = ["*"];
+  process.env.CORS_WHITELIST = ['*']
 }
