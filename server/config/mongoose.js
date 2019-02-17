717365c1d95e7b0ed0f849a70b0958e91a91c0d6
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-02-17 13:21:57 +0100

    Il comando seed verifica se il database sia popolato.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -5,6 +5,4 @@ mongoose.set('useCreateIndex', true)
 mongoose.set('useFindAndModify', false)
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/archivio', { useNewUrlParser: true })
 
-module.exports = {
-  mongoose
-}
+module.exports = mongoose
--- server/config/seed/seed.js
+++ server/config/seed/seed.js
@@ -1,16 +1,31 @@
-const { yellow, green, red } = require("colors");
+const { yellow, green, red } = require('colors')
+const mongoose = require('../mongoose')
 
-const loader = require("../loader/loader");
-
-loader
-  .then(messages => {
-    for (let message in messages) {
-      console.log(yellow(messages[message]));
+mongoose.connection.once('open', async () => {
+  mongoose.connection.db.listCollections().toArray((err, collections) => {
+    if (err) {
+      console.log(red(err.message))
+      return process.exit()
     }
-    console.log(green("Il database è stato popolato."));
-    return process.exit();
+
+    if (collections.length) {
+      console.log(green('Il database è già stato popolato.'))
+      return process.exit()
+    }
+
+    const loader = require('../loader/loader')
+
+    loader
+      .then(messages => {
+        for (let message in messages) {
+          console.log(yellow(messages[message]))
+        }
+        console.log(green('Il database è stato popolato.'))
+        return process.exit()
+      })
+      .catch(e => {
+        console.error(red(e.message))
+        return process.exit()
+      })
   })
-  .catch(e => {
-    console.error(red(e.message));
-    return process.exit();
-  });
+})
