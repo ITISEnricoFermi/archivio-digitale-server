6a156555b5351b902757b3bbfe54196f7529926e
Ernesto Montada Romero <e.montada0@gmail.com>
2019-03-24 07:57:09 +0100

    todo

--- server/resolvers/resolvers.js
+++ server/resolvers/resolvers.js
@@ -1,22 +1,5 @@
 module.exports = {
   Query: {
-    user: (root, { firstname }, context) => {
-      return {
-        firstname: firstname ? firstname : "Hello",
-        lastname: "String",
-        email: "String",
-        password: "String",
-        accesses: [
-          {
-            id: "a"
-          },
-          {
-            id: "b"
-          }
-        ],
-        privileges: "String",
-        state: "String"
-      };
-    }
+    user(root, { firstname }, context){}
   }
-};
+}
