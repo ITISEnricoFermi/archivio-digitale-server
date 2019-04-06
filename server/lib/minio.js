4ffcb7ae1506f00806eb9baa97c9b17035dd3f81
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-04-06 23:37:23 +0200

    Number port minio.

--- server/lib/minio.js
+++ server/lib/minio.js
@@ -2,7 +2,7 @@ const Minio = require('minio')
 
 module.exports = new Minio.Client({
   endPoint: process.env.MINIO_HOST || 'localhost',
-  port: process.env.MINIO_PORT || 3013,
+  port: Number(process.env.MINIO_PORT) || 3013,
   useSSL: false,
   accessKey: process.env.ACCESS_KEY || 'accesskey',
   secretKey: process.env.SECRET_KEY || 'secretkey'
