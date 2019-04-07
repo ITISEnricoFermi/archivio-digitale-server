34d2b820d065b9ce1d187878ed9209c3457f9a90
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-04-07 12:47:28 +0200

    Corretto errore Minio ENV.

--- server/lib/minio.js
+++ server/lib/minio.js
@@ -4,6 +4,6 @@ module.exports = new Minio.Client({
   endPoint: process.env.MINIO_HOST || 'localhost',
   port: Number(process.env.MINIO_PORT) || 3013,
   useSSL: false,
-  accessKey: process.env.ACCESS_KEY || 'accesskey',
-  secretKey: process.env.SECRET_KEY || 'secretkey'
+  accessKey: process.env.MINIO_ACCESS_KEY || 'accesskey',
+  secretKey: process.env.MINIO_SECRET_KEY || 'secretkey'
 })
