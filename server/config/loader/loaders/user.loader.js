ee780538534373c6c08f36427b43a6155d29c9e6
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-04-19 14:19:20 +0200

    Object storage nel seed.

--- server/config/loader/loaders/user.loader.js
+++ server/config/loader/loaders/user.loader.js
@@ -2,55 +2,32 @@ require('../../../lib/mongoose')
 
 const path = require('path')
 const fs = require('fs')
-const sharp = require('sharp')
 
 const {
   User
 } = require('../../../models/user')
 
 const users = require('../../seed/seeds/user.json')
+const uploader = require('../../../lib/uploader')
 
-const sizes = [{
-  path: 'xlg',
-  xy: 1200
-}, {
-  path: 'lg',
-  xy: 800
-}, {
-  path: 'md',
-  xy: 500
-}, {
-  path: 'sm',
-  xy: 300
-}, {
-  path: 'xs',
-  xy: 100
-}]
+const master = fs.createReadStream(path.join(__dirname, '..', '..', '..', 'assets', 'profile.svg'))
+const mimetypes = ['image/jpeg', 'image/png', 'image/gif']
+const store = uploader('image/jpeg', mimetypes)
 
 const loadUsers = new Promise((resolve, reject) => {
   return User.create(users)
-    .then(response => {
-      let files = []
-
-      for (let c = 0; c < response.length; c++) {
-        let dir = path.join(__dirname, '..', '..', '..', 'public', 'pics', String(response[c]._id))
-
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir)
-        }
-
-        for (let i = 0; i < sizes.length; i++) {
-          const file = sharp(path.join(__dirname, '..', '..', '..', 'public', 'images', 'profile.svg'))
-            .resize(sizes[i].xy, sizes[i].xy)
-            .toFormat('jpeg')
-            .toFile(path.join(__dirname, '..', '..', '..', 'public', 'pics', String(response[c]._id), sizes[i].path + '.jpeg'))
-          files.push(file)
-        }
+    .then(users => {
+      const pics = []
+      for (let i = 0; i < users.length; i++) {
+        pics.push(store.pics(master, users[i].id))
       }
-
-      return Promise.all(files)
-        .then(files => resolve('Utenti creati con successo.'))
-        .catch(e => reject(new Error('Impossibile generare le foto degli utenti.')))
+      return Promise.all(pics)
+        .then(() => {
+          resolve('Utenti creati con successo.')
+        })
+        .catch(e => {
+          reject(new Error('Impossibile generare le foto degli utenti.'))
+        })
     })
     .catch(e => reject(new Error(e)))
 })
